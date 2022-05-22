04a9be14ed7ad7685be64aed78f76c0d0ee83ec0
Martin Merck <martin.merck@gmx.de>
2022-05-22 17:44:42 +0200

    Added Blume

---
+++ Blume/blume.asm
@@ -0,0 +1,58 @@
+
+;
+; Beispiel Program aus "Rechner Modular" Seite 141ff
+; Author Rolf-Dieter Klein
+; VASM Assembler code von Dr. Martin Merck
+;
+
+    .include ../Include/z80grund.asm
+    .org $8800
+
+START:
+	jp BLUME
+
+VIERTELKREIS:
+    ld hl,90
+    call SCHLEIFE
+        ld hl,1
+		call SCHREITE
+		ld hl,90
+		call DREHE
+  	call ENDSCHLEIFE
+    ret
+
+BLATT:
+	call VIERTELKREIS
+	ld hl,90
+	call DREHE
+	call VIERTELKREIS
+	ld hl,90
+	call DREHE
+	ret
+
+BLUETE:
+	ld hl,5
+	call SCHLEIFE
+		call BLATT
+		ld hl,72
+		call DREHE
+	call ENDSCHLEIFE
+	ret
+
+BLUME:
+	call BLUETE
+	ld hl,-150
+	call SCHREITE
+	call BLATT
+	ld hl,-10
+	call SCHREITE
+	ld hl,-90
+	call DREHE
+	call BLATT
+	ld hl,90
+	call DREHE
+	ld hl,-40
+	call DREHE
+	ret
+
+	end
