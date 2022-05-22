e1e2ba6901a0f803548042f5f3be6cc7d95a0a38
Martin Merck <martin.merck@gmx.de>
2022-05-22 18:21:02 +0200

    Added Strasse

---
+++ Strasse/strasse.asm
@@ -0,0 +1,53 @@
+
+;
+; Beispiel Program aus "Rechner Modular" Seite 140
+; Author Rolf-Dieter Klein
+; VASM Assembler code von Dr. Martin Merck
+;
+    .include ../Include/z80grund.asm
+    .org $8800
+
+EINS:
+    ld hl,90
+    call DREHE
+    ld hl,20
+    call SCHREITE
+    ld hl,-90
+    call DREHE
+    ld hl,200
+    call SCHREITE
+    ld hl,-90
+    call DREHE
+    ld hl,20
+    call SCHREITE
+    ld hl,-90
+    call DREHE
+    ld hl,40
+    call SCHREITE
+    call HEBE
+    ld hl,40
+    call SCHREITE
+    call SENKE
+    ld hl,40
+    call SCHREITE
+    call HEBE
+    ld hl,40
+    call SCHREITE
+    call SENKE
+    ld hl,40
+    call SCHREITE
+    ld hl,90
+    call DREHE
+    ld hl,20
+    call SCHREITE
+    ld hl,90
+    call DREHE
+    ld hl,200
+    call SCHREITE
+    ld hl,90
+    call DREHE
+    ld hl,20
+    call SCHREITE
+
+    ret
+    end
